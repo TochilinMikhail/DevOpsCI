3246fb2fea392a5a455a4dd37821e986f3ce5465
mikhailTochilin <tochilin.mi@phystech.edu>
2022-12-04 17:22:36 +0300

    init

---
+++ setup.py
@@ -0,0 +1,63 @@
+
+from pathlib import Path
+
+from setuptools import setup
+
+NAME = 'enhancer'
+DESCRIPTION = (
+    'Speech enhancement in the waveform domain.'
+    ' Supports offline and streaming evaluation.'
+    ' For training, please directly clone the github repository.')
+
+URL = 'https://github.com/TochilinMikhail/DevOpsCI'
+EMAIL = 'mi.tochilin@gmail.com'
+AUTHOR = 'Tochilin Mikhail'
+REQUIRES_PYTHON = '>=3.7.0'
+VERSION = "0.1.5"
+
+HERE = Path(__file__).parent
+
+REQUIRED = [
+    'julius',
+    'hydra_core<1.0',
+    'hydra_colorlog<1.0',
+    'numpy>=1.19',
+    'pystoi>=0.3.3',
+    'six',
+    'sounddevice>=0.4',
+    'torch>=1.5',
+    'torchaudio>=0.5',
+]
+
+REQUIRED_LINKS = [
+    "git+https://github.com/ludlows/python-pesq#egg=pesq",
+]
+
+try:
+    with open(HERE / "README.md", encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=['enhancer'],
+    install_requires=REQUIRED,
+    dependency_link=REQUIRED_LINKS,
+    include_package_data=True,
+    license='Public Domain',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Topic :: Multimedia :: Sound/Audio :: Speech',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
